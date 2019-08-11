29076be8826812443e6d6d0ccd5cf3b03841ab7e
ashakirzianov <ashakirzianov@icloud.com>
2019-08-10 18:33:56 -0700

    Move index to root folder

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
-import { path2book } from './epub';
-import { VolumeNode } from './bookFormat';
-import { preprocessBook } from './preprocessBook';
+import { path2book } from './src/epub';
+import { VolumeNode } from './src/bookFormat';
+import { preprocessBook } from './src/preprocessBook';
 
 export async function loadEpubPath(path: string): Promise<VolumeNode> {
     const book = await path2book(path);
