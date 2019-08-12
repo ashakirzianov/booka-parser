2da1164cee0e5745910201f7d031d242e4efdd85
ashakirzianov <ashakirzianov@icloud.com>
2019-08-12 14:11:26 -0700

    Manualy set parser version

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { path2book } from './epub';
 import { VolumeNode } from './bookFormat';
 import { preprocessBook } from './preprocessBook';
-import * as pkg from '../package.json';
 
-export const parserVersion = pkg.version;
+export const parserVersion = '1.1.2';
 export async function loadEpubPath(path: string): Promise<VolumeNode> {
     const book = await path2book(path);
     const preprocessed = preprocessBook(book.value);
