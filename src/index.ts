632a2da383685d107798d20bc1bab9636d839bf2
ashakirzianov <ashakirzianov@icloud.com>
2019-08-12 14:05:33 -0700

    Export parser version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { path2book } from './epub';
 import { VolumeNode } from './bookFormat';
 import { preprocessBook } from './preprocessBook';
+import * as pkg from '../package.json';
 
+export const parserVersion = pkg.version;
 export async function loadEpubPath(path: string): Promise<VolumeNode> {
     const book = await path2book(path);
     const preprocessed = preprocessBook(book.value);
