cae505c2281f32bdec5f4fb953fafefdd404215a
ashakirzianov <ashakirzianov@icloud.com>
2019-08-31 17:39:33 +0700

    Improve image data resolution

--- src/preprocessBook/storeBuffers.ts
+++ src/preprocessBook/storeBuffers.ts
@@ -1,25 +1,36 @@
 import { VolumeNode, BookContentNode } from 'booka-common';
 import { filterUndefined } from '../utils';
 
-export type StoreBufferFn = (buffer: Buffer) => Promise<string | undefined>;
+export type StoreBufferFn = (buffer: Buffer, id: string) => Promise<string | undefined>;
 export async function storeBuffers(volume: VolumeNode, fn: StoreBufferFn): Promise<VolumeNode> {
+    const processed = await processNodes(volume.nodes, {
+        fn, resolved: {},
+    });
     return {
         ...volume,
-        nodes: await processNodes(volume.nodes, fn),
+        nodes: processed,
     };
 }
 
-async function processNodes(nodes: BookContentNode[], fn: StoreBufferFn): Promise<BookContentNode[]> {
-    const result = await Promise.all(nodes.map(n => processNode(n, fn)));
+type StoreBufferEnv = {
+    fn: StoreBufferFn,
+    resolved: {
+        [key: string]: string | undefined,
+    },
+};
+async function processNodes(nodes: BookContentNode[], env: StoreBufferEnv): Promise<BookContentNode[]> {
+    const result = await Promise.all(nodes.map(n => processNode(n, env)));
 
     return filterUndefined(result);
 }
 
-async function processNode(node: BookContentNode, fn: StoreBufferFn): Promise<BookContentNode | undefined> {
+async function processNode(node: BookContentNode, env: StoreBufferEnv): Promise<BookContentNode | undefined> {
     switch (node.node) {
         case 'image-data':
-            const url = await fn(node.data);
+            const stored = env.resolved[node.id];
+            const url = stored || await env.fn(node.data, node.id);
             if (url) {
+                env.resolved[node.id] = url;
                 return {
                     node: 'image-url',
                     id: node.id,
@@ -31,7 +42,7 @@ async function processNode(node: BookContentNode, fn: StoreBufferFn): Promise<Bo
         case 'chapter':
             return {
                 ...node,
-                nodes: await processNodes(node.nodes, fn),
+                nodes: await processNodes(node.nodes, env),
             };
         default:
             return node;
