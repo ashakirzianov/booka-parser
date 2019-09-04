ddb83116a1e76529dd4c8a2ab453394d7cec1461
ashakirzianov <ashakirzianov@icloud.com>
2019-09-04 17:10:10 +0700

    Ignore 'big'

--- src/epub/hooks.gutenberg.ts
+++ src/epub/hooks.gutenberg.ts
@@ -6,7 +6,7 @@ import { ParserDiagnoser } from '../log';
 export const gutenbergHooks: EpubConverterHooks = {
     nodeHooks: [
         // TODO: do not ignore?
-        ignoreTags(['hr', 'blockquote', 'table', 'br']),
+        ignoreTags(['hr', 'blockquote', 'table', 'br', 'big']),
     ],
     metadataHooks: [metaHook],
 };
