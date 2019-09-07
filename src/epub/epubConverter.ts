fe9f5663caa60474e9352a5ae51b3bf64a27aedd
ashakirzianov <ashakirzianov@icloud.com>
2019-09-07 16:00:06 +0700

    Extract metadata parsing

--- src/epub/epubConverter.ts
+++ src/epub/epubConverter.ts
@@ -1,12 +1,11 @@
 import { Book, KnownTag, RawBookNode } from 'booka-common';
-import { EpubBook } from './epubParser.types';
-import {
-    AsyncIter, flatten, equalsToOneOf,
-} from '../utils';
+import { AsyncIter, equalsToOneOf } from '../utils';
 import { buildVolume } from '../buildVolume';
-import { EpubConverterParameters, EpubConverter, EpubConverterResult, MetadataHook, MetadataRecord } from './epubConverter.types';
-import { ParserDiagnoser, diagnoser } from '../log';
+import { diagnoser } from '../log';
+import { EpubBook } from './epubParser.types';
+import { EpubConverterParameters, EpubConverter, EpubConverterResult } from './epubConverter.types';
 import { parseSections } from './sectionParser';
+import { parseMeta } from './metaParser';
 
 export function createConverter(params: EpubConverterParameters): EpubConverter {
     return {
@@ -22,10 +21,10 @@ async function convertEpub(epub: EpubBook, params: EpubConverterParameters): Pro
         }
 
         const hooks = params.options[epub.kind];
+        const tags = parseMeta(epub.metadata, hooks.metadataHooks, ds);
         const sections = await AsyncIter.toArray(epub.sections());
         const rawNodes = parseSections(sections, hooks.nodeHooks, ds);
-        const tags = buildMetaTags(epub, hooks.metadataHooks, ds);
-        const metaNodes = buildMetaNodes(tags);
+        const metaNodes = buildMetaNodesFromTags(tags);
         const allNodes = rawNodes.concat(metaNodes);
 
         const volume = await buildVolume(allNodes, {
@@ -55,75 +54,11 @@ async function convertEpub(epub: EpubBook, params: EpubConverterParameters): Pro
     }
 }
 
-function buildMetaNodes(tags: KnownTag[]): RawBookNode[] {
+function buildMetaNodesFromTags(tags: KnownTag[]): RawBookNode[] {
     const filtered = tags.filter(t => equalsToOneOf(t.tag, ['author', 'title', 'cover-ref']));
     const nodes = filtered.map(t => ({
         node: 'tag',
         tag: t,
     } as const));
     return nodes;
-}
-
-function defaultMetadataHook({ key, value }: MetadataRecord): KnownTag[] | undefined {
-    switch (key) {
-        case 'title':
-            return [{ tag: 'title', value }];
-        case 'creator':
-            return [{ tag: 'author', value }];
-        case 'cover':
-            return [{ tag: 'cover-ref', value }];
-        case 'subject':
-            return [{ tag: 'subject', value }];
-        case 'language':
-            return [{ tag: 'language', value }];
-        case 'publisher':
-            return [{ tag: 'publisher', value }];
-        case 'description':
-            return [{ tag: 'description', value }];
-        case 'series':
-            return [{ tag: 'series', value }];
-        case 'ISBN':
-            return [{ tag: 'ISBN', value }];
-        case 'dc:rights':
-            return [{ tag: 'rights', value }];
-        case 'creatorFileAs':
-        case 'date':
-        case 'dc:identifier':
-            return [];
-        default:
-            return undefined;
-    }
-}
-
-function buildMetaTags(epub: EpubBook, metadataHooks: MetadataHook[], ds: ParserDiagnoser): KnownTag[] {
-    const allHooks = metadataHooks.concat(defaultMetadataHook);
-    const result: KnownTag[] = [];
-    for (const [key, value] of Object.entries(epub.metadata)) {
-        if (Array.isArray(value)) {
-            const tags = flatten(
-                value.map(v => buildMetaTagsForRecord(key, v, allHooks, ds))
-            );
-            result.push(...tags);
-        } else if (value) {
-            const tags = buildMetaTagsForRecord(key, value, allHooks, ds);
-            result.push(...tags);
-        }
-    }
-
-    return result;
-}
-
-function buildMetaTagsForRecord(key: string, value: string, allHooks: MetadataHook[], ds: ParserDiagnoser): KnownTag[] {
-    const result: KnownTag[] = [];
-    const record = { key, value };
-    const tags = allHooks.reduce<KnownTag[] | undefined>(
-        (res, hook) => res || hook(record, ds),
-        undefined,
-    );
-    if (!tags) {
-        ds.add({ diag: 'unknown-meta', key, value });
-        return [];
-    } else {
-        return tags;
-    }
 }
---
+++ src/epub/metaParser.ts
@@ -0,0 +1,75 @@
+import { KnownTag, RawBookNode } from 'booka-common';
+import { EpubMetadata } from './epubParser.types';
+import { flatten, equalsToOneOf } from '../utils';
+import { MetadataHook, MetadataRecord } from './epubConverter.types';
+import { ParserDiagnoser } from '../log';
+
+export function parseMeta(meta: EpubMetadata, hooks: MetadataHook[], ds: ParserDiagnoser): KnownTag[] {
+    const tags = buildMetaTags(meta, hooks, ds);
+
+    return tags;
+}
+
+function defaultMetadataHook({ key, value }: MetadataRecord): KnownTag[] | undefined {
+    switch (key) {
+        case 'title':
+            return [{ tag: 'title', value }];
+        case 'creator':
+            return [{ tag: 'author', value }];
+        case 'cover':
+            return [{ tag: 'cover-ref', value }];
+        case 'subject':
+            return [{ tag: 'subject', value }];
+        case 'language':
+            return [{ tag: 'language', value }];
+        case 'publisher':
+            return [{ tag: 'publisher', value }];
+        case 'description':
+            return [{ tag: 'description', value }];
+        case 'series':
+            return [{ tag: 'series', value }];
+        case 'ISBN':
+            return [{ tag: 'ISBN', value }];
+        case 'dc:rights':
+            return [{ tag: 'rights', value }];
+        case 'creatorFileAs':
+        case 'date':
+        case 'dc:identifier':
+            return [];
+        default:
+            return undefined;
+    }
+}
+
+function buildMetaTags(meta: EpubMetadata, metadataHooks: MetadataHook[], ds: ParserDiagnoser): KnownTag[] {
+    const allHooks = metadataHooks.concat(defaultMetadataHook);
+    const result: KnownTag[] = [];
+    for (const [key, value] of Object.entries(meta)) {
+        if (Array.isArray(value)) {
+            const tags = flatten(
+                value.map(v => buildMetaTagsForRecord(key, v, allHooks, ds))
+            );
+            result.push(...tags);
+        } else if (value) {
+            const tags = buildMetaTagsForRecord(key, value, allHooks, ds);
+            result.push(...tags);
+        }
+    }
+
+    return result;
+}
+
+function buildMetaTagsForRecord(key: string, value: string, allHooks: MetadataHook[], ds: ParserDiagnoser): KnownTag[] {
+    const result: KnownTag[] = [];
+    const record = { key, value };
+    const tags = allHooks.reduce<KnownTag[] | undefined>(
+        (res, hook) => res || hook(record, ds),
+        undefined,
+    );
+    if (!tags) {
+        ds.add({ diag: 'unknown-meta', key, value });
+        return [];
+    } else {
+        return tags;
+    }
+}
