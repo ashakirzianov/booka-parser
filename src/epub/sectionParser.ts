95893d7b6d66199a21873c4813d80b25ea50cbd4
ashakirzianov <ashakirzianov@icloud.com>
2019-09-07 15:52:01 +0700

    Extract section parsing

---
+++ src/epub/sectionParser.ts
@@ -0,0 +1,232 @@
+import { ChapterTitle, RawBookNode } from 'booka-common';
+import {
+    XmlNode, makeStream, path, children, choice,
+} from '../xml';
+import { isWhitespaces } from '../utils';
+import { ParserDiagnoser } from '../log';
+import {
+    EpubNodeParserEnv, handleXml, constrainElement, EpubNodeParser, fullParser,
+} from './nodeParser';
+import { EpubSection } from './epubParser.types';
+
+export function parseSections(sections: EpubSection[], hooks: EpubNodeParser[], ds: ParserDiagnoser) {
+    const handlers = hooks.concat(standardHandlers);
+    const singleParser = choice(...handlers);
+    const sectionParser = buildSectionParser(singleParser);
+
+    const result: RawBookNode[] = [];
+    for (const section of sections) {
+        const env: EpubNodeParserEnv = {
+            ds: ds,
+            filePath: section.filePath,
+            nodeParser: singleParser,
+        };
+        const nodes = section.content.type === 'document'
+            ? section.content.children
+            : [section.content];
+        const stream = makeStream(nodes, env);
+        const sectionResult = sectionParser(stream);
+        if (sectionResult.success) {
+            result.push(...sectionResult.value);
+        } else {
+            ds.add({ diag: 'couldnt-parse-section', filePath: section.filePath });
+        }
+    }
+
+    return result;
+}
+
+function buildSectionParser(parser: EpubNodeParser) {
+    const bodyParser = children(fullParser(parser));
+    const resultParser = path(['html', 'body'], bodyParser);
+
+    return resultParser;
+}
+
+const text = handleXml(node => {
+    if (node.type !== 'text') {
+        return undefined;
+    }
+    // Skip whitespace nodes
+    if (node.text.startsWith('\n') && isWhitespaces(node.text)) {
+        return { node: 'ignore' };
+    } else {
+        return {
+            node: 'span',
+            span: node.text,
+        };
+    }
+});
+
+const italic = constrainElement(['em', 'i'], {}, (el, env) => ({
+    node: 'attr',
+    attributes: ['italic'],
+    content: buildContainerNode(el.children, env),
+}));
+
+const bold = constrainElement(['strong', 'b'], {}, (el, env) => ({
+    node: 'attr',
+    attributes: ['bold'],
+    content: buildContainerNode(el.children, env),
+}));
+
+const quote = constrainElement('q', { class: null }, (el, env) => {
+    return {
+        node: 'attr',
+        attributes: ['quote'],
+        content: buildContainerNode(el.children, env),
+    };
+});
+
+const a = constrainElement(
+    'a',
+    {
+        class: null, href: null,
+        id: null, title: null, tag: null,
+    },
+    (el, env) => {
+        if (el.attributes.href !== undefined) {
+            return {
+                node: 'ref',
+                to: el.attributes.href,
+                content: buildContainerNode(el.children, env),
+            };
+        } else if (el.attributes.id !== undefined) {
+            return {
+                node: 'container',
+                ref: `${env.filePath}#${el.attributes.id}`,
+                nodes: [buildContainerNode(el.children, env)],
+            };
+        } else {
+            return { node: 'ignore' };
+        }
+    });
+
+const pph = constrainElement(
+    ['p', 'div', 'span'],
+    {
+        class: null, id: null,
+        'xml:space': null, // TODO: handle ?
+    },
+    (el, env) => {
+        const container = buildContainerNode(el.children, env);
+        const result: RawBookNode = el.attributes.id
+            ? {
+                node: 'container',
+                ref: `${env.filePath}#${el.attributes.id}`,
+                nodes: [container],
+            }
+            : container;
+        return result;
+    });
+
+const img = constrainElement(
+    'img',
+    { src: null, alt: null, class: null },
+    (el, env) => {
+        const src = el.attributes['src'];
+        if (src) {
+            return {
+                node: 'image-ref',
+                imageId: src,
+            };
+        } else {
+            env.ds.add({
+                diag: 'img-must-have-src',
+                node: el,
+            });
+            return { node: 'ignore' };
+        }
+    });
+
+const image = constrainElement('image', {}, (el, env) => {
+    const xlinkHref = el.attributes['xlink:href'];
+    if (xlinkHref) {
+        return {
+            node: 'image-ref',
+            imageId: xlinkHref,
+        };
+    } else {
+        env.ds.add({ diag: 'image-must-have-xlinkhref', node: el });
+        return { node: 'ignore' };
+    }
+});
+
+const header = constrainElement(
+    ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+    { id: null },
+    (el, env) => {
+        const level = parseInt(el.name[1], 10);
+        const title = extractTitle(el.children, env.ds);
+        if (title.length === 0) {
+            env.ds.add({ diag: 'no-title', node: el });
+        }
+        return {
+            node: 'title',
+            title: title,
+            level: 4 - level,
+        };
+    });
+
+const svg = constrainElement(
+    'svg',
+    { viewBox: null, xmlns: null, class: null },
+    () => ({ node: 'ignore' })
+);
+
+const rest = constrainElement(
+    ['sup', 'sub', 'ul', 'li', 'br'], // TODO: do not ignore 'br'
+    {},
+    (el, env) => {
+        return { node: 'ignore' };
+    });
+
+const standardHandlers = [
+    text, italic, bold, quote,
+    a, pph, img, image, header,
+    svg, rest,
+];
+
+function buildContainerNode(nodes: XmlNode[], env: EpubNodeParserEnv): RawBookNode {
+    const parser = fullParser(env.nodeParser);
+    const stream = makeStream(nodes, env);
+    const result = parser(stream);
+
+    return {
+        node: 'container',
+        nodes: result.value,
+    };
+}
+
+function extractTitle(nodes: XmlNode[], ds: ParserDiagnoser): ChapterTitle {
+    const lines: string[] = [];
+    for (const node of nodes) {
+        switch (node.type) {
+            case 'text':
+                if (!isWhitespaces(node.text)) {
+                    lines.push(node.text);
+                }
+                break;
+            case 'element':
+                switch (node.name) {
+                    case 'em': case 'strong': case 'big':
+                    case 'a': case 'b':
+                    case 'span': case 'div': case 'p':
+                        const fromElement = extractTitle(node.children, ds);
+                        lines.push(fromElement.join(''));
+                        break;
+                    case 'br':
+                        break;
+                    default:
+                        ds.add({ diag: 'unexpected-node', node, context: 'title' });
+                        break;
+                }
+                break;
+            default:
+                ds.add({ diag: 'unexpected-node', node, context: 'title' });
+                break;
+        }
+    }
+
+    return lines;
+}
