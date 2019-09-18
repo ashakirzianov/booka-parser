d344a250a4421f46e38eba7dd05b4e1a0cdadbbf
ashakirzianov <ashakirzianov@icloud.com>
2019-09-18 14:35:23 +0700

    Implement 'fileHash'

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -1,5 +1,6 @@
 import { createHash } from 'crypto';
 import { Book, extractNodeText } from 'booka-common';
+import { createReadStream } from 'fs';
 
 export function bookHash(book: Book) {
     const input = extractNodeText(book.volume);
@@ -9,7 +10,21 @@ export function bookHash(book: Book) {
 }
 
 export function bufferHash(buffer: Buffer) {
-    return createHash('sha1')
+    return createHash('md5')
         .update(buffer)
         .digest('base64');
+}
+
+export function fileHash(filePath: string) {
+    return new Promise<string>((resolve, reject) => {
+        try {
+            const hash = createHash('md5');
+            const stream = createReadStream(filePath);
+
+            stream.on('data', data => hash.update(data));
+            stream.on('end', () => resolve(hash.digest('base64')));
+        } catch (e) {
+            reject(e);
+        }
+    });
 }
