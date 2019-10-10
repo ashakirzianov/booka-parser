a9c464bdb390193f5a19de193aabbc94bc868c4f
ashakirzianov <ashakirzianov@icloud.com>
2019-10-10 16:25:01 +0700

    Remove 'simplify' & 'optimize' book

--- src/preprocessBook/preprocess.ts
+++ src/preprocessBook/preprocess.ts
@@ -1,13 +1,9 @@
 import { Book } from 'booka-common';
-import { optimizeBook } from './optimizeBook';
-import { simplifyBook } from './simplifyBook';
 import { storeBuffers, ProcessImagesArgs } from './storeBuffers';
 
 export async function preprocessBook(book: Book, env: ProcessImagesArgs): Promise<Book> {
-    const simplified = simplifyBook(book);
-    const optimized = optimizeBook(simplified);
     const resolved = env.restoreBuffer
-        ? await storeBuffers(optimized, env)
-        : optimized;
+        ? await storeBuffers(book, env)
+        : book;
     return resolved;
 }
