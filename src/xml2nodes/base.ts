c6720d2d805ed64ad2e1a5b27e736c84d071feec
ashakirzianov <ashakirzianov@icloud.com>
2019-10-10 11:06:42 +0700

    Remove 'ResultNode'

--- src/xml2nodes/base.ts
+++ src/xml2nodes/base.ts
@@ -3,16 +3,9 @@ import { XmlTree, tree2String } from '../xmlStringParser';
 import { BookContentNode } from 'booka-common';
 import { isWhitespaces } from '../utils';
 
-export type IgnoreNode = { node: 'ignore' };
-export type ResultNode = BookContentNode | IgnoreNode;
 export type Env = {};
 export type Input = Stream<XmlTree, Env>;
 export type NodeParser = StreamParser<XmlTree, BookContentNode[], Env>;
-export type SingleNodeParser = StreamParser<XmlTree, ResultNode, Env>;
-
-export function isContent(node: ResultNode): node is BookContentNode {
-    return node.node !== 'ignore';
-}
 
 export function expectEmptyContent(children: XmlTree[]): ParserDiagnostic {
     return children.length > 0
